607682087dd2fa88234de3fb5ff0d19a360d6b6c
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-07-18 11:30:09 -0700

    Handling command lines with arguments

---
+++ 2.execve.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - executing a program with execve
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char cmd[] = "bin/ls";
+	char *argv[] = {"ls", "-l", NULL};
+	char *env[] = {NULL};
+
+	if (execve(cmd, argv, env) == -1)
+	{
+		perror("Error");
+	}
+	return (0);
+}
