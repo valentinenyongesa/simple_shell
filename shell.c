5c16c964c232c4ef6101b5ab6bbfad8dfd69328b
valentinenyongesa <valentinenyongesa94@gmail.com>
2023-07-19 13:36:21 -0700

    Writing a UNIX command line interpreter

--- shell.c
+++ shell.c
@@ -3,65 +3,59 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
-#include <stdbool.h>
 #include <sys/stat.h>
-
-#include "main.h"
 
 #define BUFFER_SIZE 1024
 
 /**
  * main - write UNIX command line interpreter
- * @argc: argument count
- * @argv: argument vector
- * @env: the environ
  *
  * Return: 0
  */
 
-int main(int argc, char *argv[], char **env)
+int main(void)
 {
 	char *buf = NULL;
 	char *new = "#cisfun$ ";
 	size_t buf_size = 0;
-	ssize_t n;
-	int i;
-	bool pipe = false;
 	pid_t pid;
-	(void)argc;
-	(void)argv;
+	ssize_t read;
+	int s;
+	int i = 0;
+	char cmd[] = "/bin/ls";
+	char *arg[] = {"ls", NULL};
+	char *env[] = {NULL};
 
-	while (1 && !pipe)
+	while (1)
 	{
-		if (isatty(STDIN_FILENO) == 0)
-			pipe = true;
 		write(STDOUT_FILENO, "#cisfun$ ", 9);
-
-		n = getline(&buf, &buf_size, stdin);
-		if (n == -1)
+		read = getline(&buf, &buf_size, stdin);
+		if (read != -1 && buf[read - 1] == '\n')
+			buf[read - 1] = '\0';
+		char *token = strtok(buf, " ");
+		char *args[BUFFER_SIZE];
+		while (token != NULL)
 		{
-			perror("Error (getline)");
-			free(buf);
-			exit(EXIT_FAILURE);
+			args[i++] = token;
+			token = strtok(NULL, " ");
 		}
-		if (buf[n - 1] == '\n')
-			buf[n - 1] = '\0';
-
+		args[i] = NULL;
+		if (strcmp(args[0], "exit") == 0)
+			break;
 		pid = fork();
-
-		if (pid == -1)
-		{
-			perror("Error (fork)");
-			exit(EXIT_FAILURE);
-		}
 		if (pid == 0)
-			execve(buf, &new, env);
-		if (waitpid(pid, &i, 0) == -1)
 		{
-			perror("Error (wait)");
-			exit(EXIT_FAILURE);
+			if (execve(cmd, arg, env) < 0)
+			{
+				perror("error execve");
+			}
 		}
+		else
+		{
+			while (wait(&s) != pid)
+				;
+		}
+
 	}
 	free(buf);
-	return (0);
 }
